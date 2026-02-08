9a6c9f1815743695e3e0a7316e5974c46f4288bd
Saber Brasher <134096880+saberbrasher@users.noreply.github.com>
2026-02-08 00:07:04 +0000

    added basic code and dependencies.

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Options Monitor", layout="wide")
+
+st.title("Options Activity Monitor")
+st.write("👋 If you can see this, Streamlit is working.")
